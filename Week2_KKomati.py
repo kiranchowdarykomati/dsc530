5bb79359a64b38212fb29647678317a2b721850c
kiranchowdarykomati <kiran.y6@gmail.com>
2021-06-20 23:12:48 -0400

    Week 2

---
+++ Week2_KKomati.py
@@ -0,0 +1,14 @@
+print('Hello World! I wonder why that is always the default coding text to start with')
+print(200+100)
+print(200-100)
+print(200*100)
+print(200/100)
+print('Bellevue'+' University')
+
+lista = ['one','two','three',4]
+print(lista)
+lista.append(5)
+print(lista)
+
+tuplea = ('w','x','y','z')
+print(tuplea)
